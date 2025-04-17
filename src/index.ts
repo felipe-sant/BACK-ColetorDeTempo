6ca1c71b7f81bb0223cc7e52fecc7c3e693eb6d6
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-04-17 16:24:48 -0300

    feat: adicionando dependencia de .env

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import databaseRouters from './routes/database.routes';
 
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3001
+const dbUrl = process.env.API || "nao funcionou"
 
-app.use(cors());
-app.use(express.json());
+console.log(dbUrl)
 
-app.use('/api', databaseRouters)
+// app.use(cors());
+// app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// app.use('/api', databaseRouters)
+
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
