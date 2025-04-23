bcb5ec3b9e4b58a76c7106cb43d9164ec5bdc7f0
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-04-23 19:29:07 -0300

    feat: adicionando rota de listagem de dados

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -7,6 +7,21 @@ class DatabaseService {
         const response = await novoClima.save()
         return response
     }
+
+    async getClima() {
+        const response = await Clima.find().sort({ timestamp: -1 }).limit(1)
+        return response[0]
+    }
+
+    async getClimas(dataInicio: Date, dataFinal: Date) {
+        const response = await Clima.find({
+            timestamp: {
+                $gte: dataInicio,
+                $lte: dataFinal
+            }
+        }).sort({ timestamp: 1 })
+        return response
+    }
 }
 
 export default DatabaseService;
